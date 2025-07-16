b6981537363cfcd21f174a864308a943c68693e3
chinnawat paksupho <chinnawatpaksupho@gmail.com>
2025-07-16 15:06:13 +0700

    api login binding

---
+++ index.js
@@ -0,0 +1,22 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const body = {
+        username,
+        password,
+    };
+
+    fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    })
+    .then(res => res.json())
+    .then(res => {
+        if (res.accessToken) {
+            alert("Login Successfully");
+        } else {
+            alert(res.message);
+        }
+    })
+};
